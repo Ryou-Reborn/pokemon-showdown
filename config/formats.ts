51572c75e8e5d5e34d27acd155c6b7d88b1a1341
Ryou-Reborn <78808099+Ryou-Reborn@users.noreply.github.com>
2021-07-04 20:45:10 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -266,6 +266,23 @@ export const Formats: FormatList = [
 		}
 	},
 	{
+		name: "[Gen 8] Desert Field Singles",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666169/">OU Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666247/">OU Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666340/">OU Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		onBegin(){
+			this.add('-fieldstart', 'move: Desert Field');
+			this.field.terrain = 'desertfield' as ID;
+			this.field.terrainData = {id: 'desertfield'};
+			this.add('-message', 'The field is rife with sand.');
+		}
+	},
+	{
 		section: "Pokemon Reborn Doubles",
 	},
 	{
@@ -518,6 +535,24 @@ export const Formats: FormatList = [
 			this.field.terrain = 'snowymountainfield' as ID;
 			this.field.terrainData = {id: 'snowymountainfield'};
 			this.add('-message', 'The snow glows white on the mountain…');
+		}
+	},
+	{
+		name: "[Gen 8] Desert Field Doubles",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666169/">OU Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666247/">OU Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666340/">OU Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		gameType: 'doubles',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		onBegin(){
+			this.add('-fieldstart', 'move: Desert Field');
+			this.field.terrain = 'desertfield' as ID;
+			this.field.terrainData = {id: 'desertfield'};
+			this.add('-message', 'The field is rife with sand.');
 		}
 	},
 	{
@@ -1060,6 +1095,27 @@ export const Formats: FormatList = [
 			this.add('-message', 'The snow glows white on the mountain…');
 		}
 	},
+	{
+		name: "[Gen 8] Custom Game (Desert)",
+		mod: 'gen8',
+		searchShow: false,
+		debug: true,
+		maxLevel: 9999,
+		battle: {trunc: Math.trunc},
+		defaultLevel: 100,
+		teamLength: {
+			validate: [1, 24],
+			battle: 24,
+		},
+		// no restrictions, for serious (other than team preview)
+		ruleset: ['Team Preview', 'Cancel Mod'],
+		onBegin(){
+			this.add('-fieldstart', 'move: Desert Field');
+			this.field.terrain = 'desertfield' as ID;
+			this.field.terrainData = {id: 'desertfield'};
+			this.add('-message', 'The field is rife with sand.');
+		}
+	},
 	// Sw/Sh Doubles
 	///////////////////////////////////////////////////////////////////
 
@@ -1548,6 +1604,28 @@ export const Formats: FormatList = [
 			this.field.terrain = 'snowymountainfield' as ID;
 			this.field.terrainData = {id: 'snowymountainfield'};
 			this.add('-message', 'The snow glows white on the mountain…');
+		}
+	},
+	{
+		name: "[Gen 8] Doubles Custom Game (Desert)",
+		mod: 'gen8',
+		gameType: 'doubles',
+		searchShow: false,
+		debug: true,
+		maxLevel: 9999,
+		battle: {trunc: Math.trunc},
+		defaultLevel: 100,
+		teamLength: {
+			validate: [1, 24],
+			battle: 24,
+		},
+		// no restrictions, for serious (other than team preview)
+		ruleset: ['Team Preview', 'Cancel Mod'],
+		onBegin(){
+			this.add('-fieldstart', 'move: Desert Field');
+			this.field.terrain = 'desertfield' as ID;
+			this.field.terrainData = {id: 'desertfield'};
+			this.add('-message', 'The field is rife with sand.');
 		}
 	},
 	// National Dex
